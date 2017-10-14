7588c29638b90378c31c410a222fd1d6bd41bbec
ksfee684 <kato@ksfee.org>
2017-10-14 20:26:54 +0900

    Implement functions to limit the number of viewing videos

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 
 This is a chrome extension to destroy tabs that loading `youtube.com`.
 
+We can limit the number of viewing `youtube.com` videos.
+
 ## LICENSE
 
 MIT
--- src/destroyer.js
+++ src/destroyer.js
@@ -1,6 +1,44 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    if (changeInfo.url !== undefined && changeInfo.url.match('youtube.com')) {
-        chrome.tabs.remove(tabId);
-    }
+    chrome.storage.local.get(['current', 'limit'], (function(items) {
+        let {current=0, limit=10} = items;
+
+        if (changeInfo.url === undefined || !changeInfo.url.match('youtube.com/watch')) {
+            return;
+        }
+
+        if (current > limit) {
+            chrome.tabs.remove(tabId);
+        }
+
+        chrome.storage.local.set({'current': current + 1}, (function() {
+            console.info(`current update to ${current + 1}`);
+        }));
+    }));
+});
+
+$(function() {
+    $('#limit').change(function() {
+        let limit = Number($('#limit').val()) || 10;
+        console.log('save limit ' + limit);
+        chrome.storage.local.set({'limit': limit}, (function() {
+            console.info(`limit update to ${limit}`);
+        }));
+    });
+});
+
+$(function() {
+    $('#clear').click(function() {
+        chrome.storage.local.set({'current': 0}, (function() {
+            console.info('current update to 0');
+        }));
+    });
+});
+
+$(document).ready(function() {
+    chrome.storage.local.get(['current', 'limit'], (function(items) {
+        let {current=0, limit=10} = items;
+        $('#limit').val(limit);
+        console.log(`limit: ${limit}`);
+    }));
 });
 
